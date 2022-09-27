88abc23bccbeffd011b2fa41e6ca256207ae46ca
郭晓东 <guoxiaodong@corp.netease.com>
2022-09-27 16:03:46 +0800

    feature:retrofit使用

--- app/src/main/java/com/gxd/demo/coroutine/network/RetrofitObj.kt
+++ app/src/main/java/com/gxd/demo/coroutine/network/RetrofitObj.kt
@@ -1,8 +1,11 @@
 package com.gxd.demo.coroutine.network
 
 import com.gxd.demo.coroutine.network.transform.ResponseConverterFactory
+import okhttp3.OkHttpClient
+import okhttp3.Protocol
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitObj {
     val retrofit: Retrofit by lazy {
@@ -10,6 +13,15 @@ object RetrofitObj {
             .baseUrl("https://www.wanandroid.com/")
             .addConverterFactory(ResponseConverterFactory())
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+    private val okHttpClient: OkHttpClient by lazy {
+        OkHttpClient().newBuilder()
+            .protocols(listOf(Protocol.HTTP_1_1, Protocol.HTTP_2))
+            .connectTimeout(5, TimeUnit.SECONDS)
+            .readTimeout(5, TimeUnit.SECONDS)
+            .writeTimeout(5, TimeUnit.SECONDS)
             .build()
     }
 }
