c89f995d619dcd653a59763d77e1047899f14b41
郭晓东 <guoxiaodong@corp.netease.com>
2024-07-18 18:01:35 +0800

    optimize:日志

--- app/src/main/java/com/gxd/demo/coroutine/LogExt.kt
+++ app/src/main/java/com/gxd/demo/coroutine/LogExt.kt
@@ -1,24 +1,36 @@
 package com.gxd.demo.coroutine
 
+import kotlinx.coroutines.Job
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-/**
- * isLongLog:receiver长度大于15为true
- */
-fun Any.log(result: Any? = null, isLongLog: Boolean = false) {
+fun Job.log(suffix: String) {
+    "isActive = $isActive, isCancelled = $isCancelled, isCompleted = $isCompleted".tableFormat(16).log(suffix)
+}
+
+
+fun Any.log(result: Any? = null) {
     val time = SimpleDateFormat("mm:ss:SSS", Locale.getDefault()).format(Date())
+
     val threadInfo = Thread.currentThread().name
     val lastIndex = threadInfo.lastIndexOf('-')
-    val formattedThreadInfo = if (lastIndex == -1) {
+    val formatThreadInfo = if (lastIndex == -1) {
         threadInfo
     } else {
         val secondLastIndex = threadInfo.lastIndexOf('-', lastIndex - 1)
         threadInfo.substring(secondLastIndex + 1)
     }
-    val formattedMsg = if (result?.toString().isNullOrEmpty()) "" else "      result = $result"
-    val afterThreadInfoSpaceRepeat = " ".repeat(25 - formattedThreadInfo.length)
-    val afterThisSpacerRepeat = " ".repeat(if (isLongLog) 35 else 15 - this.toString().length)
-    println("$time\t$formattedThreadInfo$afterThreadInfoSpaceRepeat${this}$afterThisSpacerRepeat$formattedMsg")
+
+    val message = "${time.tableFormat(3)}${formatThreadInfo.tableFormat(6)}${this.toString().tableFormat()}$result"
+
+    println(message)
 }
+
+fun String.tableFormat(tableCount: Int = 5): String {
+    val repeatCount = tableCount - this.length / 4
+    val separator = "\t".accumulate(if (repeatCount < 0) 0 else repeatCount)
+    return "$this$separator"
+}
+
+fun String.accumulate(repeatCount: Int): String = if (repeatCount == 0) "" else this + accumulate(repeatCount - 1)
