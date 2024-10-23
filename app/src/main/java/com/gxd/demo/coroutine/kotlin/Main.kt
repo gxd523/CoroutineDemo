c8cccbd3411c6359adba5cc707e7b28e64b691b7
郭晓东 <guoxiaodong@corp.netease.com>
2024-10-23 15:30:34 +0800

    feature:补充示例

--- app/src/main/java/com/gxd/demo/coroutine/kotlin/Main.kt
+++ app/src/main/java/com/gxd/demo/coroutine/kotlin/Main.kt
@@ -11,21 +11,27 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newFixedThreadPoolContext
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
 import java.util.concurrent.Executors
+import kotlin.random.Random
 import kotlin.time.Duration
 
 val singleDispatcher = Executors.newSingleThreadExecutor { Thread(it, "SingleThread") }.asCoroutineDispatcher()
@@ -46,6 +52,167 @@ fun main(): Unit = runBlocking {
     "end".log()
 }
 
+suspend fun channelCase(capacity: Int = 3) = coroutineScope {
+    val channel = Channel<Int>(capacity)
+    launch(singleDispatcher) {
+        repeat(capacity) {
+            channel.send(it)
+            "send".log(it)
+        }
+        channel.close()// 别忘了Channel用完要关闭
+    }
+    launch(fixedDispatcher) {
+        delay(1_000)
+        for (result in channel) {
+            "receive1".log(result)
+            delay(1_000)
+        }
+    }
+    launch(Dispatchers.IO) {
+        delay(1_000)
+        for (result in channel) {
+            "receive2".log(result)
+            delay(2_000)
+        }
+    }
+}
+
+suspend fun flowCase() = coroutineScope {
+    val flow = flow {
+        repeat(2) {
+            val value = Random.nextInt(99)
+            emit(value)
+            "emit".log(value)
+        }
+    }.map {
+        it.toString()
+    }.onStart {
+        "onStart".log()
+    }.onCompletion {
+        "onCompletion".log(it)
+    }.catch {
+        "catch".log(it)
+    }.flowOn(singleDispatcher)
+
+    launch(Dispatchers.IO) {
+        flow.collect { "collect1".log(it) }
+    }
+    launch(fixedDispatcher) {
+        flow.collect { "collect2".log(it) }
+    }
+}
+
+suspend fun channelAsFlowCase(capacity: Int = 3) = coroutineScope {
+    val channel = Channel<Int>(capacity)
+
+    launch(Dispatchers.IO) {
+        repeat(capacity) {
+            channel.send(it)
+            "send".log(it)
+        }
+        channel.close()// 别忘了Channel用完要关闭
+    }
+
+    val flow = channel.receiveAsFlow()
+
+    launch(singleDispatcher) {
+        flow.collect {
+            "collect1".log(it)
+        }
+    }
+    launch(fixedDispatcher) {
+        flow.collect {
+            "collect2".log(it)
+        }
+    }
+}
+
+suspend fun channelFlowCase() = coroutineScope {
+    val flow = channelFlow {
+        repeat(2) {
+            val value = Random.nextInt(99)
+            launch(Dispatchers.IO) {
+                send(value)
+                "send".log(value)
+            }
+        }
+    }
+
+    launch(singleDispatcher) {
+        flow.collect {
+            delay(1_000)
+            "collect1".log(it)
+        }
+    }
+
+    launch(fixedDispatcher) {
+        flow.collect {
+            delay(2_000)
+            "collect2".log(it)
+        }
+    }
+}
+
+suspend fun sharedFlowCase(replay: Int = 0, extraBufferCapacity: Int = 3) = coroutineScope {
+    val sharedFlow = MutableSharedFlow<Int>(replay, extraBufferCapacity)
+    launch(Dispatchers.IO) {
+        repeat(replay + extraBufferCapacity) {
+            launch(Dispatchers.IO) {
+                val value = Random.nextInt(99)
+                sharedFlow.emit(value)
+                "emit".log(value)
+            }
+            delay(1_000)
+        }
+    }
+
+    launch(singleDispatcher) {
+//        delay(1)
+        withTimeout(5_000) {// collect所在的协程取消才能结束SharedFlow
+            sharedFlow.collect {
+                "collect1".log(it)
+                delay(1_000)
+            }
+        }
+    }
+
+    launch(fixedDispatcher) {
+        delay(500)
+        withTimeout(5_000) {// collect所在的协程取消才能结束SharedFlow
+            sharedFlow.collect {
+                "collect2".log(it)
+                delay(1_500)
+            }
+        }
+    }
+}
+
+suspend fun stateFlowCase() = coroutineScope {
+    val stateFlow = MutableStateFlow<Int>(1)
+    launch(singleDispatcher) {
+        repeat(5) {
+            val value = Random.nextInt(99)
+            stateFlow.emit(value)
+            "emit".log(value)
+            delay(1_000)
+        }
+    }
+
+    launch(Dispatchers.IO) {
+        delay(2_000)
+        stateFlow.collect {
+            "collect1".log(it)
+        }
+    }
+
+    launch(fixedDispatcher) {
+        delay(3_000)
+        stateFlow.collect {
+            "collect2".log(it)
+        }
+    }
+}
+
 fun <T> Flow<T>.throttle(time: Duration): Flow<T> = flow {
     var lastTime = 0L
     this@throttle.collect {
@@ -55,63 +222,6 @@ fun <T> Flow<T>.throttle(time: Duration): Flow<T> = flow {
             lastTime = currentTime
         }
     }
-}
-
-suspend fun sharedFlowCase() {
-    val sharedHotFlow = MutableStateFlow(0)
-    coroutineScope {
-        "2".log()
-        launch(Dispatchers.IO) {
-            repeat(5) {
-                "emit".log(it)
-                sharedHotFlow.emit(it)
-                delay(500)
-            }
-        }
-
-        "sss".log()
-        launch {
-            "3".log()
-            sharedHotFlow.collect {
-                "a".log(it)
-            }
-        }
-        delay(1_000)
-
-        launch {
-            "4".log()
-            sharedHotFlow.collect {
-                "b".log(it)
-            }
-        }
-    }
-    "eee".log()
-}
-
-suspend fun flowCase() {
-    val intFlow = flow {
-        fetchData(1_000, 1)
-        emit(1)
-        emit(2)
-        emit(3)
-        throw Exception("Div 0")
-    }.map {
-        "map".log(it.toString())
-        it.toString()
-    }.catch { throwable ->
-        "flow exception".log(throwable)
-    }.flowOn(Dispatchers.IO).onStart {
-        "flow start".log()
-    }.onCompletion { throwable ->// 加了catch{}，throwable就为null
-        "flow completion".log(throwable)
-    }
-
-    intFlow.collect { result -> "collect".log(result) }
-}
-
-suspend fun fetchData(delay: Long, returnData: Int): Int {
-    delay(delay)
-    return returnData
 }
 
 fun CoroutineScope.lazyLaunchCase() {
